19588bdf99f9b153f9250eff0bf39f99db434ef4
vladislav.poklonski <poklonski.vladislav@gmail.com>
2024-02-01 19:58:29 +0100

    add the argument handler

--- main.go
+++ main.go
@@ -9,6 +9,28 @@ import (
 func main() {
 	rootCmd := cmd.RootCommand()
 
+	if len(os.Args) > 0 {
+		commandName := os.Args[1]
+		if commandName == "pokemon" {
+			if len(os.Args) == 2 {
+				fmt.Println("Please enter pokemon name")
+			}
+
+			if len(os.Args) == 3 {
+				pokemonName := os.Args[1]
+				fmt.Println(pokemonName)
+				//create http request and ets
+			}
+		}
+		if commandName == "attack" {
+			//run command with function
+		}
+
+		if commandName == "stats" {
+			//export data
+		}
+	}
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
